d4a2f1d6788a86bf9862ba319bc82a2db94295f4
deepak-207 <mallyadeepak85@gmail.com>
2018-06-13 23:42:36 -0700

    new file is added

---
+++ assignment2.py
@@ -0,0 +1,30 @@
+# Q1
+
+print ("my name is deepak")
+
+#Q2
+
+print("Acad + " view")
+
+# Q3
+
+x=input("enter any number")
+y=input("enter any number")
+z=input("enter any number")
+print(x,y,z)
+
+
+#Q4
+
+s="acadview"
+course="python"
+fees=6000
+print("%s %s %d(s,course,fees))
+print(s =" " + course + " " + str(fees))
+
+#Q6
+
+name="tony stark"
+salary=100000
+print(%s, %d) (____,_____)
+print(name %s,salary %d)
