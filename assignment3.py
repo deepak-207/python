1a30b757ba28d4265902610a10143f4d2b5984c5
deepak-207 <mallyadeepak85@gmail.com>
2018-06-14 02:25:18 -0700

    new file is added

---
+++ assignment3.py
@@ -0,0 +1,64 @@
+# Q1
+l=[1,3,10,6,2]
+a=input(" enter the value")
+b=(" enter the value")
+c=(" enter the value")
+d=(" enter the value")
+e=(" enter the value")
+l=[a,b,c,d,e]
+print(l)
+
+#Q2
+
+l2=['google',' apple',' facebook',' microsoft',' tesla']
+print(l.extend(l2))
+
+print()l
+
+#Q3
+
+l=[1,2,3,1,4,1,5,1]
+print(l.count(l))
+
+#Q4
+
+l=[33,1,66,44,22]
+l.sort()
+print(l)
+
+#Q5
+
+A=[1,2,3,7,10]
+B=[5,6,8,9]
+print(A.extend(B))
+C=A
+C.sort()
+print(C)
+
+
+#Q6
+
+# for stack
+
+a=input("enter value")
+b=input("enter value")
+c=input("enter value")
+d=input("enter value")
+e=input("enter value")
+l=[a,b,c,d,e]
+printl.POP())
+PRINT(l)
+
+#for queue
+
+a=input(" enter value")
+b=input(" enter value")
+c=input(" enter value")
+d=input(" enter value")
+e=input(" enter value")
+l=[a,b,c,d,e]
+
+l.reverse()
+print(l.pop())
+
+print(l)
